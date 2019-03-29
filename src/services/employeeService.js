090b54da147c92c5248876d7a4d0ed5582fd4cab
Tai <taidao90@gmail.com>
2019-03-28 22:30:01 -0700

    Added express service

--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const express = require('express');
 
 const con = mysql.createConnection({
   host: "138.68.9.254",
@@ -8,11 +9,16 @@ const con = mysql.createConnection({
   database: "employees"
 });
 
+// Initialize MySQL Connection
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected to database!");
 });
 
+// Initialize App
+const app = express();
+
+// SQL Query
 let sqlQuery = `
 SELECT emp_no,
        first_name,
@@ -24,22 +30,29 @@ SELECT emp_no,
 FROM   employees
        NATURAL JOIN emp_info_by_dept NATURAL JOIN latestSalaryPerEmp;`
 
+app.get('/employees', function (req, res) {
+  var employeesList = [];
+  con.query(sqlQuery, function(err, rows, fields) {
+      if (err) throw err;
+  
+      for (var i = 0;i < rows.length; i++) {
+          var employee = [];
+          employee = {   
+              employee_id: rows[i].emp_no, 
+              name: rows[i].first_name+ " " + rows[i].last_name,
+              department_id: rows[i].dept_no,
+              department_name: rows[i].dept_name,
+              hiringDate: rows[i].date,
+              salary: rows[i].salary
+          }
+          employeesList.push(employee)
+      }
+      res.send(employeesList);
+      con.end();
+  });
+});
 
-var employeesList = [];
-con.query(sqlQuery, function(err, rows, fields) {
-    if (err) throw err;
-
-    for (var i = 0;i < rows.length; i++) {
-        var employee = [];
-        employee = {   
-            employee_id: rows[i].emp_no, 
-            name: rows[i].first_name+ " " + rows[i].last_name,
-            department_id: rows[i].dept_no,
-            department_name: rows[i].dept_name,
-            hiringDate: rows[i].date,
-            salary: rows[i].salary
-        }
-        employeesList.push(employee)
-    }
-    console.log(employeesList);
+// Start the server for MySQL
+app.listen(3000, () => {
+  console.log('Go to http://localhost:3000/employees to see employees');
 });
